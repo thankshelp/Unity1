ff623cb66c81c741741d9953e9f87fb8916475d6
thankshelp <taraninav.44@gmail.com>
2019-12-11 02:12:17 +0700

    f

---
+++ FirstGame/Assets/Script/move.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class move : MonoBehaviour
+{
+    public float speed;
+    public int objects;
+
+    private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
+    public Text LoseText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        winText.text = "";
+        LoseText.text = "";
+        SetCountText();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if(rb.position.y < -2.588)
+        {
+            LoseText.text = "Game over!";
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= objects)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
